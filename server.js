5fd6bc3cecddb3cd8e8ce4492e70122fc64de52f
Julian Leon <jleon1992@gmail.com>
2020-08-05 19:02:09 -0400

    express middleware added

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
+// const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
 const server = express();
 
+server.use(express.json())
+
+
+server.use(logger)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
+
+server.use('/api/users', userRouter)
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+    console.log(`You made a ${req.method} request to the  ${req.url} endpoint at ${new Date()}`)
+    next()
+}
 
 module.exports = server;
