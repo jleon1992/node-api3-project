7a795b976c99313caefe7ce39d8155240e5622bb
Julian Leon <jleon1992@gmail.com>
2020-08-06 17:34:10 -0400

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||4000;
 
 server.listen(port, () => {
     console.log('server running on port 4000')
